a77e93e4b2e2d4f2a53d4c8c3a830a1f93a32981
GuGuJelly <jellypuding@naver.com>
2024-08-29 09:40:54 +0900

    탱크 무브 보충
    보충

--- Assets/Scripts/TankMove.cs
+++ Assets/Scripts/TankMove.cs
@@ -22,6 +22,9 @@ public class TankMove : MonoBehaviour
         if (moveDir == Vector3.zero)
             return;
 
+        // moveDir.sqrMagnitude > 1 을 할 경우, 벡터의 크기의 제곱, 루트 계산을 하지 않음. 최적화에 더 좋다.
+        // magnitude : 벡터의 크기
+
         transform.Translate(moveDir.normalized * moveSpeed * Time.deltaTime, Space.World);
 
         Quaternion lookRot = Quaternion.LookRotation(moveDir);
